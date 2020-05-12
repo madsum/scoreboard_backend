7fa5dbc77b074bb01fa7ebac9113aec2149ddeba
Masum Islam <muhammed.islam@ericsson.com>
2020-05-12 08:33:24 +0300

    Add ScoreBoard model

---
+++ src/main/java/com/example/scoreboard/model/ScoreBoard.java
@@ -0,0 +1,47 @@
+package com.example.scoreboard.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "scoreboard")
+public class ScoreBoard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    private String team;
+
+    @NotNull
+    private int point;
+
+    public ScoreBoard(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+}
