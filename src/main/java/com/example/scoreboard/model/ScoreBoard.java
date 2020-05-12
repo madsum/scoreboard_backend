83040d3385719ad382cd004b655f88870c437e71
Masum Islam <muhammed.islam@ericsson.com>
2020-05-12 11:33:50 +0300

    Remove unique constraint

--- src/main/java/com/example/scoreboard/model/ScoreBoard.java
+++ src/main/java/com/example/scoreboard/model/ScoreBoard.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "scoreboard", uniqueConstraints={@UniqueConstraint(columnNames ={"team"})})
+@Table(name = "scoreboard")
 public class ScoreBoard {
 
     @Id
