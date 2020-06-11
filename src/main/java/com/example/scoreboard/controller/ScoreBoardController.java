e28cc129f627e42fa3c0d005dce20a47de7190a3
Masum Islam <muhammed.islam@ericsson.com>
2020-06-11 15:55:50 +0300

    Update post and unit test

--- src/main/java/com/example/scoreboard/controller/ScoreBoardController.java
+++ src/main/java/com/example/scoreboard/controller/ScoreBoardController.java
@@ -38,8 +38,8 @@ public class ScoreBoardController {
 
     @PostMapping(path = "/add/score")
     public ResponseEntity<?> addOrUpdate(@RequestBody ScoreBoard scoreBoard) {
-        boardRepository.save(scoreBoard);
-        return new ResponseEntity<>("ScoreBoard submission successfully", HttpStatus.OK);
+        ScoreBoard  savedScoreBoard = boardRepository.save(scoreBoard);
+        return new ResponseEntity<ScoreBoard>(savedScoreBoard, HttpStatus.OK);
     }
 
     @PutMapping(path = "/update/score")
